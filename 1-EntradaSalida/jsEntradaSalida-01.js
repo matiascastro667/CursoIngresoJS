89af3fd0bad9dceeb0e5916485fa80ca98c889f9
Your Name <you@example.com>
2021-07-06 21:35:52 -0300

    primera clase, primer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Chau Mundo 2035");
+	alert("waka waka ee e");
 //error linea 7
 
 }
